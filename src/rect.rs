aa57a3d7cc49b524f7d7bad6bf38e0e643b1ab95
Lance Hart <me@lancehart.ca>
2024-07-01 16:04:41 -0400

    Updated rect

--- src/rect.rs
+++ src/rect.rs
@@ -10,25 +10,31 @@ impl<'a> Rect<'a> {
     }
 
     /// Sets the x position
-    pub fn x(mut self, cx: f32) -> Self {
-        self.0.add_attribute("cx", cx);
+    pub fn x(mut self, x: f32) -> Self {
+        self.0.add_attribute("x", x);
         self
     }
 
     /// Sets the y position
-    pub fn y(mut self, cy: f32) -> Self {
-        self.0.add_attribute("cy", cy);
+    pub fn y(mut self, y: f32) -> Self {
+        self.0.add_attribute("y", y);
         self
     }
 
     /// Sets the width
-    pub fn width(mut self, r: f32) -> Self {
-        self.0.add_attribute("r", r);
+    pub fn width(mut self, width: f32) -> Self {
+        self.0.add_attribute("width", width);
         self
     }
 
     /// Sets the height
-    pub fn height(mut self, fill: &str) -> Self {
+    pub fn height(mut self, height: f32) -> Self {
+        self.0.add_attribute("height", height);
+        self
+    }
+
+    /// Sets the fill
+    pub fn fill(mut self, fill: &str) -> Self {
         self.0.add_attribute("fill", fill);
         self
     }
